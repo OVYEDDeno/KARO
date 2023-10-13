fa0f2096710fe555f25a683fd488d4bcd9687dc9
OVYED Deno <nevad34@gmail.com>
2023-10-13 17:16:52 +0000

    10/13

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,6 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Game
 from api.utils import generate_sitemap, APIException
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 
 api = Blueprint('api', __name__)
 def FightMove(creator_move, receiver_move):
@@ -47,22 +46,4 @@ def login():
     access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
 
-@api.route('/create-game', methods=['POST'])
-@jwt_required()
-def create_game():
-
-    body = request.get_json(force=True)
-    current_user = User.query.filter_by(email=get_jwt_identity())
-    wager = body['wager']
-
-    if current_user is None:
-        return 'User not found', 404
-    if wager is None:
-        return 'Wager needed', 400
-    
-    game= Game(creator_id=current_user['id'], wager=wager) 
-    db.session.add(game)
-    db.session.commit()
-    response_body={'message':'Successful'}
-    return jsonify(response_body), 200
 
